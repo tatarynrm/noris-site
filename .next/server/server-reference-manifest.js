98ae848a80b8d6cddb41c1f7b5cf311b28df043c
tatarynrm <tatarynrm@gmail.com>
2025-03-22 17:54:32 +0200

    1234677777

--- .next/server/server-reference-manifest.js
+++ .next/server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"+1zxeUbuSgwCsapzUj7nN7eyHr+gQJ53Bge3adqvkok=\"\n}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"process.env.NEXT_SERVER_ACTIONS_ENCRYPTION_KEY\"}"
