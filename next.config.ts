4d461b9f0b9e2d02fb64536bc22017e927038f51
tatarynrm <tatarynrm@gmail.com>
2025-03-22 18:06:43 +0200

    ewqqeweqweqweqw

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,7 @@
 import { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true, // Ігнорувати ESLint помилки під час збірки
-  },
+
 };
 
 export default nextConfig;
